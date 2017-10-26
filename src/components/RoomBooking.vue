8e2f74644425b6a4fd0653954f20099499f0c832
eric wang <eric.w385@gmail.com>
2017-10-26 18:28:01 +0800

    select time

--- src/components/RoomBooking.vue
+++ src/components/RoomBooking.vue
@@ -5,7 +5,7 @@
             <q-tab slot="title" label="全部教室" name="tab-1" />
             <q-tab slot="title" label="我的预约" name="tab-2" />
             <!-- Targets -->
-            <q-tab-pane  default name="tab-1">
+            <q-tab-pane name="tab-1">
                 <q-card>
                 <q-card-main>
                     <span slot="subtitle">
@@ -37,19 +37,93 @@
                             </q-item-main>
                         </q-item>
                     </q-list>
-                    <q-card-actions>
-                        <q-btn class="full-width" loader flat>预约</q-btn>
-                    </q-card-actions>
+                    <q-collapsible label="预约">
+                        <div>
+                            <q-datetime 
+                            v-model="data" 
+                            type="date" 
+                            float-label="Date"
+                            />
+                            
+                            <q-datetime-range
+                            type="time"
+                            format24h
+                            v-model="range"
+                            :min="mintime"
+                            :max="maxtime"
+                            float-label="Select Time"
+                            />
+                            <br>
+                            <q-btn color="primary">
+                                确认
+                            </q-btn>
+                        </div>
+                    </q-collapsible>
                 </q-card>                
             </q-tab-pane>
             <q-tab-pane name="tab-2">
-                
+                <q-card>
+                    <q-card-title>
+                        306
+                    </q-card-title>
+                    <q-list>
+                        <q-item>
+                            <q-item-side>
+                                <q-item-tile color="primary" icon="bookmark" />
+                            </q-item-side>
+                            <q-item-main>
+                                <q-item-tile label>教室类别</q-item-tile>
+                                <q-item-tile sublabel>本科人才协同培养中心</q-item-tile>   
+                            </q-item-main>
+                        </q-item>
+                        <q-item>
+                            <q-item-side>
+                                <q-item-tile color="primary" icon="event seat" />
+                            </q-item-side>
+                            <q-item-main>
+                                <q-item-tile label>教室容量</q-item-tile>
+                                <q-item-tile sublabel>80</q-item-tile>
+                            </q-item-main>
+                        </q-item>
+                        <q-item>
+                            <q-item-side>
+                                <q-item-tile color="primary" icon="event" />
+                            </q-item-side>
+                            <q-item-main>
+                                <q-item-tile label>预约日期</q-item-tile>
+                                <q-item-tile sublabel>{{data}}</q-item-tile>
+                            </q-item-main>
+                        </q-item>
+                        <q-item>
+                            <q-item-side>
+                                <q-item-tile color="primary" icon="query builder" />
+                            </q-item-side>
+                            <q-item-main>
+                                <q-item-tile label>预约时间段</q-item-tile>
+                                <q-item-tile sublabel>{{range.from}}-{{range.to}}</q-item-tile>
+                            </q-item-main>
+                        </q-item>
+                    </q-list>
+                </q-card>
             </q-tab-pane>
         </q-tabs>
     </div>
 </template>
 <script>
+import '../themes/docs-input.styl'
+
 export default {
+  data () {
+    return {
+      range: {
+        from: null,
+        to: null
+      },
+      mintime: '6:00',
+      maxtime: '18:00',
+      data: null
+    }
+  }
 }
 </script>
 
