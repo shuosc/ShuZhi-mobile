766050a1fb25951356ee9110deb1a17c8a79d6f2
Yiqun Zhao <cosformula@qq.com>
2018-01-03 09:07:14 +0800

    * fixinput

--- src/components/MInput.vue
+++ src/components/MInput.vue
@@ -2,7 +2,7 @@
   <div class="q-if row no-wrap items-center relative-position q-input q-if-has-label text-primary">
     <div class="q-if-inner col row no-wrap items-center relative-position">
       <div class="q-if-label ellipsis full-width absolute self-start q-if-label-above">{{stackLabel}}</div>
-      <input v-model="name" class="col q-input-target text-left">
+      <input v-model="value" class="col q-input-target text-left">
     </div>
   </div>
 </template>
