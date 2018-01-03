d580f3167ac44ea2f2f4265922c4e12bc3090184
Yiqun Zhao <cosformula@qq.com>
2018-01-03 09:22:14 +0800

    * format

--- src/components/MInput.vue
+++ src/components/MInput.vue
@@ -2,10 +2,7 @@
   <div class="q-if row no-wrap items-center relative-position q-input q-if-has-label text-primary">
     <div class="q-if-inner col row no-wrap items-center relative-position">
       <div class="q-if-label ellipsis full-width absolute self-start q-if-label-above">{{stackLabel}}</div>
-      <input ref="input" 
-      v-bind:value="value" 
-      class="col q-input-target text-left" 
-      v-on:input="updateValue($event.target.value)"/>
+      <input ref="input" v-bind:value="value" class="col q-input-target text-left" v-on:input="updateValue($event.target.value)" />
     </div>
   </div>
 </template>
@@ -14,7 +11,7 @@
 export default {
   props: ['value', 'stack-label'],
   methods: {
-    updateValue: function (value) {
+    updateValue: function(value) {
       this.$emit('input', value)
     }
   }
